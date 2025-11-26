b23321a7f9af985e2ad6dccd042e6d011db4d46f
MR-RITZY <mrboffin01@gmail.com>
2025-11-26 04:56:12 +0100

    Resolving SSL cert issue in Redis

--- src/stu_house_market/redis_manager.py
+++ src/stu_house_market/redis_manager.py
@@ -1,6 +1,5 @@
 from redis.asyncio import Redis, ConnectionPool, SSLConnection, Connection
 from src.stu_house_market.config import settings
-from ssl import CERT_REQUIRED
 from typing import Optional, Any
 from contextlib import asynccontextmanager
 
@@ -24,7 +23,7 @@ connection_kwargs = dict(
 if settings.ENV == "PROD":
     connection_kwargs.update(
         {
-            "ssl_cert_reqs": CERT_REQUIRED,
+            "ssl_cert_reqs": None,
             "username": settings.REDIS_USERNAME,
             "password": settings.REDIS_PASSWORD,
         }
